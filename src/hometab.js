51eae06ea862edfbef43e2f4a95b8c90d330dd05
didou <didou>
2023-04-23 14:40:35 +0100

    update

--- src/hometab.js
+++ src/hometab.js
@@ -8,13 +8,14 @@ const Hometab = () => {
     return ( 
         <div className='whole'>
          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <img src={require('./Pics/logo.png')} className = "logo" alt = "logo" />
+         <img src={require('./Pics/logo.png')} className = "logo" alt = "logo" />
         <div className='section1' ref={sec1}>
             <img src = {require('./Pics/IMG_9794.png')} className="profile" alt = "profile"/>
             <h1 className="Hi">Hi!</h1>
             <h2 className="name">I'm Dhia Eddine Ounissi </h2>
             <h2 className="current"> Programer , Radiologic Technologist and a Gamer </h2>
         </div>
+
         <div className='section2'ref={sec2}>
             <h1 className= {`${sec2vis ? '' :'about'} ${sec2vis ? 'aboutvis' :''}`}>About</h1>
             <p className={`${sec2vis ? '' :'p1'} ${sec2vis ? 'p1vis' :''}`}>Welcome to my portfolio , i m dhia eddine a Computer Science student and a beginner programer , i 
@@ -27,13 +28,13 @@ const Hometab = () => {
             <h2 className={`${sec2vis ? '' :'h2-2'} ${sec2vis ? 'h2-2vis' :''}`}>What i'm learning </h2>
             <p className={`${sec2vis ? '' :'p3'} ${sec2vis ? 'p3vis' :''}`}>Backend development , Graphic design <br></br>Video editing , German and English</p>
         </div>
+
         <div className='section3'>
            <div className='section3in'ref={sec3}> 
            <h1 className= {`${sec3vis ? 'contact' :''}`}>contact</h1> 
             <a href="https://web.facebook.com/didou.archer.420/"><img className= {` ${sec3vis ? 'facebook' :''}`} src={require('./Pics/Facebook_Logo.png')}></img></a>
             <a href="https://twitter.com/didou_archer"><img className= {` ${sec3vis ? 'twitter' :''}`} src={require('./Pics/twitter_logo.png')}></img></a>
-           </div>
-           
+           </div>  
         </div>
         </div>  
      );
--- src/navbar.js
+++ src/navbar.js
@@ -2,7 +2,7 @@
 const Navbar = () => {
     return (
       <div className="nav">
-         <a className="Homenav">Home</a>
+         <a className="homenav">Home</a>
          <a className="aboutnav">About</a>
          <a className="contactnav">Contact</a>
       </div>
